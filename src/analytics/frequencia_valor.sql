d24c90ec268bc48fb586768073917992c37bd814
Gabriel Thiago <leucides123@gmail.com>
2026-05-07 22:39:59 -0300

    Categorizando os nossos 6 clusters para analisar a concentração de grupos nestes clustes

--- src/analytics/frequencia_valor.sql
+++ src/analytics/frequencia_valor.sql
@@ -1,3 +1,5 @@
+with tb_freq_valor as (
+
 SELECT
     idCliente,
     count(DISTINCT substr(DtCriacao, 0, 11)) as qtdeFrequencia,
@@ -7,5 +9,26 @@ FROM
 WHERE DtCriacao < '2025-09-01'
 AND DtCriacao >= date('2025-09-01', '-28 day')
 GROUP BY idCliente
-ORDER BY qtdeFrequencia DESC;
- 
+ORDER BY qtdeFrequencia DESC
+ ),
+
+ tb_cluster as (
+ SELECT
+    *,
+    CASE
+        WHEN qtdeFrequencia <= 10 AND qtdePontosPos > 1500 THEN 'Hypers'
+        WHEN qtdeFrequencia > 10 AND qtdePontosPos >= 1500 THEN 'Eficientes'
+        WHEN qtdeFrequencia <= 10 AND qtdePontosPos >= 750 THEN 'Indeciso'
+        WHEN qtdeFrequencia > 10 AND qtdePontosPos >= 750 THEN 'Esforçado'
+        WHEN qtdeFrequencia < 5  THEN 'Lurker'
+        WHEN qtdeFrequencia <= 10   THEN 'Preguiçoso'
+        WHEN qtdeFrequencia > 10   THEN 'Potencial'
+    END AS cluster
+
+FROM
+    tb_freq_valor
+)
+
+SELECT 
+    *
+FROM tb_cluster
